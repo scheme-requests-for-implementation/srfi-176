2f969d14aac9efd6c53ee9342954120cb63fa851
Lassi Kortela <lassi@lassi.io>
2019-12-03 16:55:53 +0200

    Disable read-time evaluation in Common Lisp reader

--- implementation/read-using-common-lisp.lisp
+++ implementation/read-using-common-lisp.lisp
@@ -6,7 +6,7 @@
             (when (or (not c) (and line-start? (char= c #\())) (return))
             (setf line-start? (char= #\newline (read-char in))))))
   (with-standard-io-syntax
-    (let ((xs '()) (eof (gensym)))
+    (let ((xs '()) (eof (gensym)) (*read-eval* nil))
       (loop (let ((x (read in nil eof)))
               (when (eql eof x) (return (nreverse xs)))
               (push x xs))))))
