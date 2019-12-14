cf9ca1c0faf0cfdcb2b8e30efb35a30f43facb21
Lassi Kortela <lassi@lassi.io>
2019-12-14 17:38:14 +0200

    Enable GCC/clang runtime buffer overflow detector

--- implementation/read-using-c-build.sh
+++ implementation/read-using-c-build.sh
@@ -3,6 +3,6 @@ set -eu
 cd "$(dirname "$0")"
 echo "Entering directory '$PWD'"
 CC=${CC:-clang}
-CFLAGS=${CFLAGS:--Werror -Wall -Wextra -ansi -pedantic}
+CFLAGS=${CFLAGS:--Werror -Wall -Wextra -ansi -pedantic -fsanitize=address}
 set -x
 $CC $CFLAGS -o read-using-c read-using-c.c read-using-c-helpers.c
